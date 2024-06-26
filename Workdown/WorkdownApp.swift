b523dd43a0f1e574e83223fbdc1806d82d194b3e
paraversal <104731964+paraversal@users.noreply.github.com>
2024-06-26 11:50:32 +0200

    Initial Commit

---
+++ Workdown/WorkdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkdownApp.swift
+//  Workdown
+//
+//  Created by jules on 26.06.24.
+//
+
+import SwiftUI
+
+@main
+struct WorkdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
